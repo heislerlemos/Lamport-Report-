932b811bfdc3d07e4f69eedf7b1b357f564bd4d3
heisler lemos <heisler.lemos@gmail.com>
2018-05-01 20:44:34 +0100

    adding tests now

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20180417101510) do
   create_table "lamport_notes", force: :cascade do |t|
     t.string "titulo"
     t.text "texto"
+    t.string "email", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
@@ -60,12 +61,11 @@ ActiveRecord::Schema.define(version: 20180417101510) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
+    t.integer "failed_attempts", default: 10, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
