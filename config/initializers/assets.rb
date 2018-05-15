fd628d9ce3373a74c5982dbaa17e104685d6b32e
heisler lemos <heisler.lemos@gmail.com>
2018-05-15 12:27:01 +0100

    styles markaby

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,6 +13,7 @@ Rails.application.config.assets.precompile += %w( eventos.css )
 Rails.application.config.assets.precompile += %w( calendarios.css )
 Rails.application.config.assets.precompile += %w( item_events.css )
 Rails.application.config.assets.precompile += %w( twitterposts.css )
+Rails.application.config.assets.precompile += %w( abouts.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
---
+++ config/initializers/markaby.rb
@@ -0,0 +1,6 @@
+require 'markaby/rails'
+
+Markaby::Rails::TemplateHandler.register!({
+  tagset: Markaby::HTML5,
+  indent: 2,
+})
