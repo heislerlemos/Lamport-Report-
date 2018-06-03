016ddea070a94f73aa9dd72858264c103a038750
heisler lemos <heisler.lemos@gmail.com>
2018-06-03 16:07:34 +0100

    fixing assets on initializer 6

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module Lamport
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.assets.enabled = true
+    config.assets.prefix = Rails.root.join('tmp/assets').to_s
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
