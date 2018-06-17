8f65c73804b74f6d949153309c165da06225f6b1
heisler lemos <heisler.lemos@gmail.com>
2018-06-17 10:07:21 +0100

    working on users creation

--- app/controllers/twitterposts_controller.rb
+++ app/controllers/twitterposts_controller.rb
@@ -1,15 +1,16 @@
 class TwitterpostsController < ApplicationController
 
   def index
-    @twitterpost = current_user.twitterposts.build
-    @twitterposts_paginação = Twitterpost.paginate(:page => params[:page], per_page: 10).where(user_id: current_user)
+    @twitterposts = Twitterpost.all
+    @twitterposts_paginação = Twitterpost.paginate(:page => params[:page], per_page: 10).order("created_at DESC")
+
 
     end
 
   def create
 
     @twitterpost = current_user.twitterposts.build(:message => params[:message])
-
+      
     respond_to do |format|
       if @twitterpost.save
         format.html { redirect_to twitterposts_path}
